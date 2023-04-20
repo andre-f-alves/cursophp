c19ac1c53492d6b11d73fe9c2e0eb7fc150dc2f3
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2023-04-20 09:51:45 -0300

    Desafio 13 solucionado

--- desafios/d013/index.php
+++ desafios/d013/index.php
@@ -44,21 +44,21 @@
 
         <?php
             $reais_100 = (int) ($valor / 100);
-            $reais_50 = (int) ($valor % 100 / 50);
-            $reais_20 = (int) ($valor % 50 / 20);
-            $reais_10 = (int) ($valor % 20 / 10);
-            $reais_5 = (int) ($valor % 10 / 5);
-            $reais_2 = $reais_5 % 2;
-            $real_1 = 0;
+            $reais_50 = (int) (($valor % 100) / 50);
+            $reais_20 = (int) (($valor % 100 % 50) / 20);
+            $reais_10 = (int) (($valor % 100 % 50 % 20) / 10);
+            $reais_5 = (int) (($valor % 100 % 50 % 20 % 10) / 5);
+            $reais_2 = (int) (($valor % 100 % 50 % 20 % 10 % 5) / 2);
+            $real_1 = (int) (($valor % 100 % 50 % 20 % 10 % 5 % 2) / 1);
         ?>
 
         <ul>
-            <li><img src="imgs/100-reais.jpg" alt="100 reais"> x<?=$reais_100?></li>
-            <li><img src="imgs/50-reais.jpg" alt="50 reais"> x<?=$reais_50?></li>
-            <li><img src="imgs/20-reais.jpg" alt="20 reais"> x<?=$reais_20?></li>
-            <li><img src="imgs/10-reais.jpg" alt="10 reais"> x<?=$reais_10?></li>
-            <li><img src="imgs/5-reais.jpg" alt="5 reais"> x<?=$reais_5?></li>
-            <li><img src="imgs/2-reais.jpg" alt="2 reais"> x<?=$reais_2?></li>
+            <li><img src="imgs/100-reais.jpg" alt="100 reais"> &times;<?=$reais_100?></li>
+            <li><img src="imgs/50-reais.jpg" alt="50 reais"> &times;<?=$reais_50?></li>
+            <li><img src="imgs/20-reais.jpg" alt="20 reais"> &times;<?=$reais_20?></li>
+            <li><img src="imgs/10-reais.jpg" alt="10 reais"> &times;<?=$reais_10?></li>
+            <li><img src="imgs/5-reais.jpg" alt="5 reais"> &times;<?=$reais_5?></li>
+            <li><img src="imgs/2-reais.jpg" alt="2 reais"> &times;<?=$reais_2?></li>
             <li><img src="imgs/1-real.jpg" alt="1 real" class="peq"> x<?=$real_1?></li>
         </ul>
     </section>
